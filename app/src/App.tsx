f60ff431e860ab0bf7a2639341e34b0240b357e2
Pitura <pa.pitura@gmail.com>
2024-04-15 20:56:37 +0200

    brb

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import kingB from './assets/kingB.svg';
+import kingW from './assets/kingW.svg';
 
 function App() {
 
   return (
     <>
       <h1>Chess</h1>
+      <div>
+        <img src={kingB} alt="king" style={{width:'4rem', height:'4rem'}}/>
+        <img src={kingW} alt="king" style={{width:'4rem', height:'4rem'}}/>
+      </div>
     </>
   )
 }
